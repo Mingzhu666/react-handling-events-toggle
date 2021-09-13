5212c8453b871de4e842d470def01e5ab1fe2fa1
万明珠 <mingzhuwan@gmail.com>
2021-09-13 17:51:19 +1000

    updates for toggle component

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import './Toggle.scss';
-
 class Toggle extends Component {
   constructor() {
     super();
 
     this.state = {
-      isToggle: true,
+      isToggleOn: false,
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState((prevState) => ({
-      isToggle: !prevState.isToggle,
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
     }));
   }
 
   render() {
-    return (
-      <button onClick={this.handleClick}>{this.state.isToggle ? "ON" : "OFF"}</button>
+    return(
+      <button onClick={this.handleClick}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
     )
-  };
-}
+  }
+} 
 
 export default Toggle;
